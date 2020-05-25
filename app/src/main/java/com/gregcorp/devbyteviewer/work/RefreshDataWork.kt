e369612f7f202e327161a4faaa62cebc7145ab62
greg <greg.verissimo@gmail.com>
2020-05-25 11:28:23 +0200

    18. Exercise: Create a Worker

--- app/src/main/java/com/gregcorp/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/gregcorp/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,26 @@
  */
 
 package com.gregcorp.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.gregcorp.devbyteviewer.database.getDatabase
+import com.gregcorp.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) : CoroutineWorker(appContext, params) {
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+
+}
